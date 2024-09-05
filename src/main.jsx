1e7d4b846b7806562cc29b00f97868ece9b45a22
KalenskiDeni <kallenski.deni@gmail.com>
2024-09-06 00:09:37 +0200

    Revert "Merge branch 'main' of https://github.com/KalenskiDeni/crud_spilcafeen_project"
    This reverts commit 2dfc83a3c77ad06bc65454ccee31a39b9454898a, reversing
    changes made to 9f8505fcc037128c511578230c4e1a666ac149cc.

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/crud_spilcafeen_project/"}>
-          <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
